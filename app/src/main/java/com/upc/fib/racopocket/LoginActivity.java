9e48ad9deb0301d3c1017811b88372a165d76a9c
Josep de Cid Rodríguez <Josep de Cid Rodrguez>
2016-06-15 00:00:06 +0200

    Removed unnecessary resources and redirected LoginActicvity

--- app/src/main/java/com/upc/fib/racopocket/LoginActivity.java
+++ app/src/main/java/com/upc/fib/racopocket/LoginActivity.java
@@ -90,7 +90,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
         signInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                Intent intent = new Intent(LoginActivity.this, MainMenuActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
