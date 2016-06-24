ab9b31ccf8b9092fc8a34bb223be4e04cee14c74
Josep Rivaille <josep.de.cid@gmail.com>
2016-06-24 20:57:18 +0200

    Added new images and launcher logo

--- app/src/main/java/com/upc/fib/racopocket/MainMenuActivity.java
+++ app/src/main/java/com/upc/fib/racopocket/MainMenuActivity.java
@@ -70,7 +70,6 @@ public class MainMenuActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
