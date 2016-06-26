4d05d9996077f389a30eeb13ce2fdd71a1037239
Josep de Cid Rodríguez <Josep de Cid Rodrguez>
2016-06-25 22:10:30 -0700

    Added autocomplete and new logics to Subject info

--- app/src/main/java/com/upc/fib/racopocket/SubjectInfoMainMenu.java
+++ app/src/main/java/com/upc/fib/racopocket/SubjectInfoMainMenu.java
@@ -8,10 +8,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,14 +24,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class SubjectInfoMainMenu extends Fragment
 {
-    EditText subjectSelector;
+    AutoCompleteTextView subjectSelector;
     Button buttonThread;
     TextView subjectName, subjectData;
     ProgressBar progressBar;
-    static final String API_URL = "https://raco.fib.upc.edu/api/assignatures/info.json?codi_upc=";
+
+    ArrayList<String> subjects_name = new ArrayList<>();
+    ArrayList<String> subjects_code = new ArrayList<>();
+
+    static final String API_URL = "https://raco.fib.upc.edu/api/assignatures";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,28 +47,108 @@ public class SubjectInfoMainMenu extends Fragment
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        subjectSelector = (EditText) view.findViewById(R.id.subjectSelector);
+        //TODO: Check if data is already in the local DB
+        new GetSubjectsInfo().execute();
+
         buttonThread = (Button) view.findViewById(R.id.queryButton);
 
         subjectName = (TextView) view.findViewById(R.id.subjectName);
         subjectData = (TextView) view.findViewById(R.id.subjectData);
+
+        subjectSelector = (AutoCompleteTextView) view.findViewById(R.id.subjectSelector);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_list_item_1, subjects_name);
+        subjectSelector.setAdapter(adapter);
 
         progressBar = (ProgressBar) view.findViewById(R.id.progressBar);
 
         buttonThread.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String subjectCode = subjectSelector.getText().toString();
-                new HttpAsyncTask().execute(subjectCode);
+                String subjectName = subjectSelector.getText().toString();
+                if (subjectName.length() == 0) {
+                    Toast.makeText(getActivity(), "Write something to serch!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean found = false;
+                    for (int i = 0; i < subjects_name.size() && !found; i++) {
+                        if (subjects_name.get(i).equals(subjectName)) {
+                            String subjectCode = subjects_code.get(i);
+                            new GetSubjectInfoWithCode().execute(subjectCode);
+                            found = true;
+                        }
+                    }
+                    if (!found)
+                        Toast.makeText(getActivity(), "This subject does not exists!", Toast.LENGTH_SHORT).show();
+                }
+                subjectSelector.setText("");
             }
         });
 
         progressBar.setVisibility(View.GONE);
         progressBar.setMax(100);
+
+
+    }
+
+    public class GetSubjectsInfo extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                URL url = new URL(API_URL + "/llista.json");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append('\n');
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally {
+                    urlConnection.disconnect();
+                }
+            } catch (Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+            progressBar.setVisibility(View.GONE);
+
+            if (response == null) {
+                subjectName.setText(getResources().getString(R.string.connection_problems));
+            }
+            else {
+                try {
+                    JSONArray allSubjectsJSON = new JSONArray(response);
+                    String data;
+                    for (int i = 0; i < allSubjectsJSON.length(); i++) {
+                        JSONObject singleSubjectJSON = allSubjectsJSON.getJSONObject(i);
+                        data = singleSubjectJSON.getString("idAssig") + " - " + singleSubjectJSON.getString("nom");
+                        subjects_name.add(data);
+                        data = singleSubjectJSON.getString("codi_upc");
+                        subjects_code.add(data);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     //AsyncTask<Params, Progress, Result>
-    public class HttpAsyncTask extends AsyncTask<String, Void, String> {
+    public class GetSubjectInfoWithCode extends AsyncTask<String, Void, String> {
 
         @Override
         protected void onPreExecute() {
@@ -76,7 +163,7 @@ public class SubjectInfoMainMenu extends Fragment
             String subjectCode = codes[0];
 
             try {
-                URL url = new URL(API_URL + subjectCode);
+                URL url = new URL(API_URL + "/info.json?codi_upc=" + subjectCode);
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
