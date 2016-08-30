1677a85ce58bd57dea099b6c02c8bbcd6cd5e051
JosepRIvaille <josep.de.cid@gmail.com>
2016-08-30 19:40:29 +0200

    Created timetable listview layout with color scheme

--- app/src/main/java/com/upc/fib/racopocket/TimetableMainMenu.java
+++ app/src/main/java/com/upc/fib/racopocket/TimetableMainMenu.java
@@ -1,20 +1,33 @@
 package com.upc.fib.racopocket;
 
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.util.Random;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TimetableMainMenu extends Fragment
 {
-    TableLayout tableLayout;
+    //ViewPager viewPager;
+    ListView listView;
+    ImageView connectionProblem;
+    ProgressBar progressBar;
+
+    HashMap<String, Integer> colorScheme;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,28 +38,139 @@ public class TimetableMainMenu extends Fragment
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        tableLayout = (TableLayout) view.findViewById(R.id.tableTimetable);
-        setTimeTable();
+        listView = (ListView) view.findViewById(R.id.listViewTimetable);
+        connectionProblem = (ImageView) view.findViewById(R.id.connectionTimetable);
+        progressBar = (ProgressBar) view.findViewById(R.id.progressBarTimetable);
+
+        setColorScheme();
+
+        new GetTimetableData().execute();
     }
 
-    private void setTimeTable() {
+    private void setColorScheme() {
+        int[] colors = {
+                Color.parseColor("#DFE9C6"),
+                Color.parseColor("#FFF3BA"),
+                Color.parseColor("#FFD2A7"),
+                Color.parseColor("#BDDCE9"),
+                Color.parseColor("#DDBFE4"),
+        };
 
-        String timetableData = FileHelpers.readFileToString(getContext().getApplicationContext(), "horari-setmanal.json");
-
-        for (int i = 0; i < 12; i++) {
-            TableRow tableRow = new TableRow(getContext());
-            tableRow.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 5f));
-            for (int j = 0; j < 5; j++) {
-                TextView textView = new TextView(getContext());
-                textView.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 12f));
-                Random rnd = new Random();
-                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-                textView.setBackgroundColor(color);
-                textView.setText("GRAU-EDA A5S102");
-                tableRow.addView(textView);
+        colorScheme = new HashMap<>();
+        String mySubjects = FileHelpers.readFileToString(getContext().getApplicationContext(), "assignatures.json");
+        try {
+            JSONArray mySubjectsJSONArray = new JSONArray(mySubjects);
+            for (int i = 0; i < mySubjectsJSONArray.length(); i++) {
+                JSONObject mySubjectJSONObject = mySubjectsJSONArray.getJSONObject(i);
+                colorScheme.put(mySubjectJSONObject.getString("idAssig"), colors[i%colors.length]);
             }
-            tableLayout.addView(tableRow);
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
+
+    private class GetTimetableData extends AsyncTask<Void, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            if (!FileHelpers.fileExists(getContext().getApplicationContext(), "horari-setmanal.json")) {
+                FileHelpers.fetchAndStoreJSONFile(getContext().getApplicationContext(), null, "https://raco.fib.upc.edu/api/aules/horari-setmanal.json" , "horari-setmanal.json");
+            }
+
+            return FileHelpers.readFileToString(getContext(), "horari-setmanal.json");
+
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+
+            if (response == null) {
+                connectionProblem.setVisibility(View.VISIBLE);
+            } else {
+                final ArrayList<TimetableInfo> classroomsInfo = new ArrayList<>();
+                try {
+                    JSONArray timetableJSONArray = new JSONArray(response);
+                    for (int i = 0; i < timetableJSONArray.length(); i++) {
+                        JSONObject currentClass = timetableJSONArray.getJSONObject(i);
+                        String subject = currentClass.getString("Assig");
+                        String group = currentClass.getString("Grup") + currentClass.getString("Tipus");
+                        String timeStart = currentClass.getString("HoraInici") + "-" + currentClass.getString("HoraFi") + "h";
+                        String classroom = currentClass.getString("Aules");
+                        classroomsInfo.add(new TimetableInfo(subject, group, timeStart, classroom));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                final ArrayAdapter<TimetableInfo> adapter = new ArrayAdapter<TimetableInfo>(getContext(), R.layout.timetable_item_list, R.id.timetableStartTime, classroomsInfo) {
+                    @Override
+                    public View getView(int position, View convertView, ViewGroup parent) {
+                        View view = super.getView(position, convertView, parent);
+                        TextView text1 = (TextView) view.findViewById(R.id.timetableStartTime);
+                        TextView text2 = (TextView) view.findViewById(R.id.timetableName);
+                        TextView text3 = (TextView) view.findViewById(R.id.timetableClassroom);
+
+                        String subject = classroomsInfo.get(position).getName();
+
+                        String name = subject + " " + classroomsInfo.get(position).getGroup();
+                        String startTime = classroomsInfo.get(position).getStartTime();
+                        String classroom = classroomsInfo.get(position).getClassroom();
+
+                        text1.setText(startTime);
+                        text2.setText(name);
+                        text3.setText(classroom);
+
+                        int color = colorScheme.get(subject);
+                        view.setBackgroundColor(color);
+
+                        return view;
+                    }
+                };
+
+                listView.setAdapter(adapter);
+            }
+
+            progressBar.setVisibility(View.GONE);
+        }
+
+    }
+
+    private class TimetableInfo {
+        String name;
+        String group;
+        String startTime;
+        String classroom;
+
+        TimetableInfo(String name, String group, String startTime, String classroom) {
+            this.name = name;
+            this.group = group;
+            this.startTime = startTime;
+            this.classroom = classroom;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String getStartTime() {
+            return this.startTime;
+        }
+
+        public String getClassroom() {
+            return this.classroom;
+        }
+
+        public String getGroup() {
+            return this.group;
+        }
+
+    }
+
 
 }
